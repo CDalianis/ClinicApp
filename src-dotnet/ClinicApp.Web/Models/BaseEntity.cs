4f198fdaec52e6571bf1112c779c462f0d13ceb4
CDalianis <christos_dl@hotmail.com>
2026-05-03 12:08:01 +0300

    feat add: init commit base entity

---
+++ src-dotnet/ClinicApp.Web/Models/BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace ClinicApp.Web.Models;
+
+public abstract class BaseEntity
+{
+    public bool IsDeleted { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
+
